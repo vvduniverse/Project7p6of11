63ce204db065d4b512f08b2f6b8f66f9defb2ff8
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-04 11:38:23 +0200

    Initial Commit

---
+++ Project7p6of11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project7p6of11
+//
+//  Created by Vahtee Boo on 04.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project7p6of11/Project7p6of11App.swift
@@ -0,0 +1,17 @@
+//
+//  Project7p6of11App.swift
+//  Project7p6of11
+//
+//  Created by Vahtee Boo on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project7p6of11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
